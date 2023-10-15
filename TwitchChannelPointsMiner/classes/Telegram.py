33af5affad83f2702c8318ddc6c688881f4855e3
Yorik <Yorik-Code@users.noreply.github.com>
2023-10-15 14:14:33 +0800

    Update Telegram.py - Telegram Message Prefix
    Add a Message Prefix to the Telegram Message.

--- TwitchChannelPointsMiner/classes/Telegram.py
+++ TwitchChannelPointsMiner/classes/Telegram.py
@@ -6,7 +6,7 @@ from TwitchChannelPointsMiner.classes.Settings import Events
 
 
 class Telegram(object):
-    __slots__ = ["chat_id", "telegram_api", "events", "disable_notification"]
+    __slots__ = ["chat_id", "telegram_api", "events", "disable_notification", "message_prefix"]
 
     def __init__(
         self, chat_id: int, token: str, events: list, disable_notification: bool = False
@@ -15,8 +15,13 @@ class Telegram(object):
         self.telegram_api = f"https://api.telegram.org/bot{token}/sendMessage"
         self.events = [str(e) for e in events]
         self.disable_notification = disable_notification
-
+        if not message_prefix or message_prefix == False:
+            self.message_prefix = ""
+        else:
+            self.message_prefix = message_prefix
+            
     def send(self, message: str, event: Events) -> None:
+        message = self.message_prefix + message
         if str(event) in self.events:
             requests.post(
                 url=self.telegram_api,
