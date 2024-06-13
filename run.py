5eb5cfd5661566294dfd09d3530608b99746ddcd
XiSZ <XiSZ@users.noreply.github.com>
2024-06-14 01:06:22 +0200

    chore: Update Docker image repository name to XiSZ/Twitch-Channel-Points-Miner-v2

--- run.py
+++ run.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
+import logging
 from colorama import Fore
 from TwitchChannelPointsMiner import TwitchChannelPointsMiner
+from TwitchChannelPointsMiner.logger import LoggerSettings, ColorPalette
 from TwitchChannelPointsMiner.classes.Chat import ChatPresence
 from TwitchChannelPointsMiner.classes.Discord import Discord
-from TwitchChannelPointsMiner.classes.Settings import Priority, Events, FollowersOrder
+from TwitchChannelPointsMiner.classes.Webhook import Webhook
 from TwitchChannelPointsMiner.classes.Telegram import Telegram
-from TwitchChannelPointsMiner.classes.entities.Bet import Strategy, BetSettings, Condition, OutcomeKeys, \
-    FilterCondition, DelayMode
+from TwitchChannelPointsMiner.classes.Matrix import Matrix
+from TwitchChannelPointsMiner.classes.Pushover import Pushover
+from TwitchChannelPointsMiner.classes.Settings import Priority, Events, FollowersOrder
+from TwitchChannelPointsMiner.classes.entities.Bet import Strategy, BetSettings, Condition, OutcomeKeys,FilterCondition, DelayMode
 from TwitchChannelPointsMiner.classes.entities.Streamer import Streamer, StreamerSettings
-from TwitchChannelPointsMiner.logger import LoggerSettings, ColorPalette
+import keep_alive,os
 
-import keep_alive,logging,os
+
 keep_alive.keep_alive()
 
 user = os.getenv('User')
@@ -28,16 +32,19 @@ twitch_miner = TwitchChannelPointsMiner(
         Priority.DROPS,   
         Priority.ORDER  
     ],
-
     enable_analytics=True,
+    disable_ssl_cert_verification=False,        # Set to True at your own risk and only to fix SSL: CERTIFICATE_VERIFY_FAILED error
+    disable_at_in_nickname=False,               # Set to True if you want to check for your nickname mentions in the chat even without @ sign
     logger_settings=LoggerSettings(
         save=True,  
         console_level=logging.INFO,
         console_username=False,
+        auto_clear=True,                        # Create a file rotation handler with interval = 1D and backupCount = 7 if True (default)
+        time_zone="Europe/Berlin",              # Set a specific time zone for console and file loggers. Use tz database names. Example: "America/Denver"
         file_level=logging.INFO,
-        emoji=False,  
+        emoji=True,  
         less=True,  
-        colored=False,  
+        colored=True,  
         color_palette=ColorPalette(  
             STREAMER_ONLINE=Fore.GREEN,  
             STREAMER_OFFLINE=Fore.RED,
@@ -51,7 +58,7 @@ twitch_miner = TwitchChannelPointsMiner(
             BET_FILTERS=Fore.MAGENTA,
             BET_GENERAL=Fore.BLUE,
             BET_FAILED=Fore.RED,
-        ),
+        ),                                                                                        # Only these events will be sent to the endpoint
         telegram=Telegram(  
             chat_id=chatID,
             token=telegramToken,
@@ -59,13 +66,16 @@ twitch_miner = TwitchChannelPointsMiner(
                 Events.STREAMER_ONLINE,
                 Events.STREAMER_OFFLINE,
                 Events.BONUS_CLAIM,
+                Events.DROP_STATUS,
                 Events.DROP_CLAIM,
+                Events.MOMENT_CLAIM,
                 Events.DROP_STATUS,
                 Events.GAIN_FOR_RAID,
                 Events.GAIN_FOR_CLAIM,
                 Events.GAIN_FOR_WATCH,
+                Events.GAIN_FOR_WATCH_STREAK,
                 Events.CHAT_MENTION
-            ], 
+            ],                                                                                  # Only these events will be sent to the endpoint
             disable_notification=True,
         ),
         discord=Discord(
@@ -74,13 +84,74 @@ twitch_miner = TwitchChannelPointsMiner(
                 Events.STREAMER_ONLINE,
                 Events.STREAMER_OFFLINE,
                 Events.BONUS_CLAIM,
+                Events.DROP_STATUS,
                 Events.DROP_CLAIM,
+                Events.MOMENT_CLAIM,
                 Events.DROP_STATUS,
                 Events.GAIN_FOR_RAID,
                 Events.GAIN_FOR_CLAIM,
                 Events.GAIN_FOR_WATCH,
+                Events.GAIN_FOR_WATCH_STREAK,
                 Events.CHAT_MENTION
             ],
+            ),                                                                                  # Only these events will be sent to the endpoint
+            webhook=Webhook(
+            endpoint="https://example.com/webhook",                                             # Webhook URL
+            method="GET",                                                                       # GET or POST
+            events=[
+                Events.STREAMER_ONLINE,
+                Events.STREAMER_OFFLINE,
+                Events.BONUS_CLAIM,
+                Events.DROP_STATUS,
+                Events.DROP_CLAIM,
+                Events.MOMENT_CLAIM,
+                Events.DROP_STATUS,
+                Events.GAIN_FOR_RAID,
+                Events.GAIN_FOR_CLAIM,
+                Events.GAIN_FOR_WATCH,
+                Events.GAIN_FOR_WATCH_STREAK,
+                Events.CHAT_MENTION
+            ],                                                                                  # Only these events will be sent to the endpoint
+            ),
+            matrix=Matrix(
+            username="twitch_miner",                                                            # Matrix username (without homeserver)
+            password="...",                                                                     # Matrix password
+            homeserver="matrix.org",                                                            # Matrix homeserver
+            room_id="...",                                                                      # Room ID
+            events=[
+                Events.STREAMER_ONLINE,
+                Events.STREAMER_OFFLINE,
+                Events.BONUS_CLAIM,
+                Events.DROP_STATUS,
+                Events.DROP_CLAIM,
+                Events.MOMENT_CLAIM,
+                Events.DROP_STATUS,
+                Events.GAIN_FOR_RAID,
+                Events.GAIN_FOR_CLAIM,
+                Events.GAIN_FOR_WATCH,
+                Events.GAIN_FOR_WATCH_STREAK,
+                Events.CHAT_MENTION
+            ],                                                                                  # Only these events will be sent
+            ),
+            pushover=Pushover(
+            userkey="YOUR-ACCOUNT-TOKEN",                                                       # Login to https://pushover.net/, the user token is on the main page
+            token="YOUR-APPLICATION-TOKEN",                                                     # Create a application on the website, and use the token shown in your application
+            priority=0,                                                                         # Read more about priority here: https://pushover.net/api#priority
+            sound="pushover",                                                                   # A list of sounds can be found here: https://pushover.net/api#sounds
+            events=[
+                Events.STREAMER_ONLINE,
+                Events.STREAMER_OFFLINE,
+                Events.BONUS_CLAIM,
+                Events.DROP_STATUS,
+                Events.DROP_CLAIM,
+                Events.MOMENT_CLAIM,
+                Events.DROP_STATUS,
+                Events.GAIN_FOR_RAID,
+                Events.GAIN_FOR_CLAIM,
+                Events.GAIN_FOR_WATCH,
+                Events.GAIN_FOR_WATCH_STREAK,
+                Events.CHAT_MENTION
+            ],                                                                                  # Only these events will be sent
         )
     ),
     streamer_settings=StreamerSettings(
@@ -112,9 +183,9 @@ twitch_miner.analytics(host="0.0.0.0", port=5050, refresh=36000, days_ago=30)  #
 
 twitch_miner.mine(
     [     
-        Streamer("warframe"),
+        Streamer("warframe",    settings=StreamerSettings(chat=ChatPresence.ONLINE)),
         Streamer("ralumyst"),
-        Streamer ("xhenniii", settings=StreamerSettings(chat=ChatPresence.ONLINE)),
+        Streamer ("xhenniii",   settings=StreamerSettings(chat=ChatPresence.ONLINE)),
         Streamer("dessyy"),
         Streamer("melvniely"), 
         Streamer("cypathic"),
