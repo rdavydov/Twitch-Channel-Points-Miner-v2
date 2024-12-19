54eea8ca4012168b61c409dd27e4d5ea7afded1d
XiSZ <XiSZ@users.noreply.github.com>
2024-12-19 01:31:53 +0100

    toggle on Emoji's in logs

--- run.py
+++ run.py
@@ -43,7 +43,7 @@ twitch_miner = TwitchChannelPointsMiner(
         auto_clear=True,                        # Create a file rotation handler with interval = 1D and backupCount = 7 if True (default)
         time_zone="Europe/Berlin",              # Set a specific time zone for console and file loggers. Use tz database names. Example: "America/Denver"
         file_level=logging.INFO,
-        emoji=False,  
+        emoji=True,  
         less=True,  
         colored=False,  
         color_palette=ColorPalette(             # Color allowed are: [BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET].
